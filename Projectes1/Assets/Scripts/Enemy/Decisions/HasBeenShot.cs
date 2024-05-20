93e1bed13ef2feb892d4cc22860d137b0b7db318
ElijahShiro <114169882+ElijahShiro@users.noreply.github.com>
2024-05-20 18:39:51 +0200

    testing area + hasbeenshot state

---
+++ Projectes1/Assets/Scripts/Enemy/Decisions/HasBeenShot.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "FSM/Decisions/HasBeenShot")]
+public class HasBeenShot : Decision
+{
+    public override bool Decide(BaseStateMachine stateMachine)
+    {
+        var HealthSystem = stateMachine.GetComponent<HealthSystem>();   
+
+        if(HealthSystem.currentHealth < HealthSystem.maxHealth)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+   
+}
