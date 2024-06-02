b3f93ae6cc0c4cbf2de8c511d7a185b2e757fd3e
ElijahShiro <114169882+ElijahShiro@users.noreply.github.com>
2024-06-02 13:44:54 +0200

    minorFix

--- Projectes1/Assets/PauseMenuBehaviour.cs
+++ Projectes1/Assets/PauseMenuBehaviour.cs
@@ -5,16 +5,19 @@ using UnityEngine.SceneManagement;
 public class PauseMenuBehaviour : MonoBehaviour
 {
     public static bool isPaused;
+    public GameObject remainingEnemies;
     private void OnEnable()
     {
         Time.timeScale = 0f;
         isPaused = true;
+        remainingEnemies.SetActive(false);
 
     }
     private void OnDisable()
     {
         Time.timeScale = 1f;
         isPaused = false;
+        remainingEnemies.SetActive(true);
     }
 
     public void RestartLevel()
